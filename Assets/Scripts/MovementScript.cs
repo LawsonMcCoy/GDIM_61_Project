1540e2795b5284372c7e3c9cd4813f29a2f27507
Pankko <alexaar3@uci.edu>
2022-04-20 20:34:53 -0700

    Movement script complete
    Finished movement script using character controller method

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 Velocity;
+    private Vector3 PlayerMovementInput;
 
-    // Update is called once per frame
+    [SerializeField] private CharacterController Controller;
+    [SerializeField] private float Speed;
+    [SerializeField] private float JumpForce;
+    [SerializeField] private float Gravity;
+
     void Update()
     {
-        
+        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        MovePlayer();
+
+    }
+    private void MovePlayer()
+    {
+        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput);
+        Controller.Move(MoveVector * Time.deltaTime);
+        Controller.Move(Velocity * Time.deltaTime);
+        if (Controller.isGrounded)
+        {
+            Velocity.y = -1;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Velocity.y = JumpForce;
+            }
+        }
+        else
+        {
+            Velocity.y -= Gravity * -2 * Time.deltaTime;
+        }
     }
 }
