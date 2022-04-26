3b298dd3c7ff3697112673a4ba71b20ffd2866f4
Pankko <alexaar3@uci.edu>
2022-04-25 21:07:47 -0700

    Added camera movement
    Added camera movement and changed the material of the plane platform to be able to tell if the player is moving or not

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -6,15 +6,23 @@ public class MovementScript : MonoBehaviour
 {
     private Vector3 Velocity;
     private Vector3 PlayerMovementInput;
+    private Vector2 PlayerMouseInput;
+    private float xRotation;
 
     [SerializeField] private CharacterController Controller;
     [SerializeField] private float Speed;
     [SerializeField] private float JumpForce;
     [SerializeField] private float Gravity;
+    [SerializeField] private Transform PlayerCamera;
+    [SerializeField] private float Sensitivity;
+
+
 
     void Update()
     {
-        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal") * Speed, 0, Input.GetAxis("Vertical") * Speed);
+        PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        MoveCamera();
         MovePlayer();
 
     }
@@ -36,4 +44,10 @@ public class MovementScript : MonoBehaviour
             Velocity.y -= Gravity * -2 * Time.deltaTime;
         }
     }
+    private void MoveCamera()
+    {
+        xRotation -= PlayerMouseInput.y * Sensitivity;
+
+        transform.Rotate(0, PlayerMouseInput.x * Sensitivity, 0);
+        PlayerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);    }
 }
