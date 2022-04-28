135275e7c3634ccb113dd6beb8738b308f0f41d9
Lawson McCoy <lawson.mccoy00@gmail.com>
2022-04-28 12:23:03 -0700

    set up a quick test with the new input system, only implemented movement

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementScript : MonoBehaviour
 {
-    private Vector3 Velocity;ddddddddddddddddddddddddddddddddddwwwwwww
+    private Vector3 Velocity;
     private Vector3 PlayerMovementInput;
     private Vector2 PlayerMouseInput;
     private float xRotation;
@@ -24,8 +25,8 @@ public class MovementScript : MonoBehaviour
     }
     void Update()
     {
-        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal") * Speed, 0, Input.GetAxis("Vertical") * Speed);
-        PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        // PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal") * Speed, 0, Input.GetAxis("Vertical") * Speed);
+        // PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         // MoveCamera();
         MovePlayer();
 
@@ -35,18 +36,18 @@ public class MovementScript : MonoBehaviour
         Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput);
         Controller.Move(MoveVector * Time.deltaTime);
         Controller.Move(Velocity * Time.deltaTime);
-        if (Controller.isGrounded)
-        {
-            Velocity.y = -1;
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Velocity.y = JumpForce;
-            }
-        }
-        else
-        {
-            Velocity.y -= Gravity * -2 * Time.deltaTime;
-        }
+        // if (Controller.isGrounded)
+        // {
+        //     Velocity.y = -1;
+        //     if (Input.GetKeyDown(KeyCode.Space))
+        //     {
+        //         Velocity.y = JumpForce;
+        //     }
+        // }
+        // else
+        // {
+        //     Velocity.y -= Gravity * -2 * Time.deltaTime;
+        // }
     }
     private void MoveCamera()
     {
@@ -55,4 +56,12 @@ public class MovementScript : MonoBehaviour
         transform.Rotate(0, PlayerMouseInput.x * Sensitivity, 0);
         PlayerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);    
     }
+
+    private void OnMovement(InputValue value)
+    {
+        Vector2 movementIn2D = value.Get<Vector2>();
+        //Note that when going from 2D vector to 3D vector, the y in 2D becomes the z in 3D, Thanks Unity
+        PlayerMovementInput = new Vector3(movementIn2D.x, 0, movementIn2D.y);
+        MovePlayer();
+    } 
 }
