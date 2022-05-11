e3533bc9083e2fd8b9aa30a0f207e6ae970c08ad
Lawson McCoy <lawson.mccoy00@gmail.com>
2022-05-10 22:12:53 -0700

    Got the jump working correctly
    Fixed a few bugs where the player was being moved and turned twice in
    certain frames

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -9,12 +9,14 @@ public class MovementScript : MonoBehaviour
     private Vector3 PlayerMovementInput;
     private Vector2 PlayerMouseInput;
     private float xRotation;
+    private Vector2 rotationValue;
 
     [SerializeField] private CharacterController Controller;
     [SerializeField] private Rigidbody playerRigidbody;
     [SerializeField] private float Speed;
     [SerializeField] private float JumpForce;
     [SerializeField] private float Gravity;
+    [SerializeField] private float TerminalVelocity;
     [SerializeField] private Transform PlayerCamera;
     [SerializeField] private float Sensitivity;
 
@@ -27,17 +29,17 @@ public class MovementScript : MonoBehaviour
 
     private void Update()
     {
-        // PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal") * Speed, 0, Input.GetAxis("Vertical") * Speed);
-        // PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        // MoveCamera();
         MovePlayer();
 
-        if (Controller.isGrounded)
-        {
-            Velocity.y -= Gravity * -2 * Time.deltaTime;
-        }
+        RotatePlayer(rotationValue);
 
+        Debug.Log(Velocity);
+        if (Velocity.y > TerminalVelocity)
+        {
+            Velocity.y += Gravity * Time.deltaTime;
+        }        
     }
+
     private void MovePlayer()
     {
         Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput);
@@ -48,11 +50,11 @@ public class MovementScript : MonoBehaviour
 
     private void OnTurning(InputValue value)
     {
-        Vector2 rotationValue = value.Get<Vector2>();
+        rotationValue = value.Get<Vector2>();
         rotationValue = rotationValue * Time.deltaTime;
         // Debug.Log($"Turning: {rotationValue}");
 
-        RotatePlayer(rotationValue);
+        // RotatePlayer(rotationValue);
     }
 
     private void MoveCamera()
@@ -76,22 +78,18 @@ public class MovementScript : MonoBehaviour
         Vector2 movementIn2D = value.Get<Vector2>();
         //Note that when going from 2D vector to 3D vector, the y in 2D becomes the z in 3D, Thanks Unity
         PlayerMovementInput = new Vector3(movementIn2D.x, 0, movementIn2D.y);
-        MovePlayer();
+        // MovePlayer();
     } 
 
     private void OnJump(InputValue value)
     {
-         if (Controller.isGrounded)
+        if (Controller.isGrounded)
         {
             Velocity.y = -1;
             if (value.isPressed)
             {
                 Velocity.y = JumpForce;
             }
-        }
-        else
-        {
-            Velocity.y -= Gravity * -2 * Time.deltaTime;
         }
     }
 
