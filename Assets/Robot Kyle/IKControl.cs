600cf3203b5333de984de16c116f7f5f2ba21cd4
Yen Chi Nguyen <nguyenyc@uci.edu>
2022-06-20 22:45:47 -0700

    created IK_test scene
    - added IK to enemy prefab basically
    - IK only works without Canvas object, Enemy and Entity scripts
    - needs to be fixed ASAP

---
+++ Assets/Robot Kyle/IKControl.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class IKControl : MonoBehaviour
+{
+
+    protected Animator animator;
+
+    public bool ikActive = false;
+    public Transform rightHandObj = null;
+    public Transform lookObj = null;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    //a callback for calculating IK
+    void OnAnimatorIK()
+    {
+        Debug.Log("testing on AnimatorIk");
+        if (animator)
+        {
+            Debug.Log("if animator");
+
+            //if the IK is active, set the position and rotation directly to the goal. 
+            if (ikActive)
+            {
+                Debug.Log("if ikAktive");
+                // Set the look target position, if one has been assigned
+                if (lookObj != null)
+                {
+                    Debug.Log("if lookObj != null");
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(lookObj.position);
+                }
+
+                // Set the right hand target position and rotation, if one has been assigned
+                if (rightHandObj != null)
+                {
+                    Debug.Log("if rightHandObj != null");
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+                }
+
+            }
+
+            //if the IK is not active, set the position and rotation of the hand and head back to the original position
+            else
+            {
+                Debug.Log("else statement");
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
+
